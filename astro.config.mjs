981431006002af59f735643c23f853b21679d9e1
chechestor <nuts3000@mail.ru>
2026-05-15 13:30:32 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://wildwildpointer.github.io',
   output: 'static',
+  base: 'site',
   integrations: [sitemap()]
 });
